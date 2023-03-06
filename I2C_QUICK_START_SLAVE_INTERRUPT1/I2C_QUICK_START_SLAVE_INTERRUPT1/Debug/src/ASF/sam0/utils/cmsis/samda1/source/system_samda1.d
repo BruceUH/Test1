3bec76d240d31e1aec2e86a05b29a465e1c0af43
bruceUH <bruce.uh@gmail.com>
2023-03-07 06:43:36 +0800

    add files

---
+++ I2C_QUICK_START_SLAVE_INTERRUPT1/I2C_QUICK_START_SLAVE_INTERRUPT1/Debug/src/ASF/sam0/utils/cmsis/samda1/source/system_samda1.d
@@ -0,0 +1,219 @@
+src/ASF/sam0/utils/cmsis/samda1/source/system_samda1.d \
+ src/ASF/sam0/utils/cmsis/samda1/source/system_samda1.o: \
+ ../src/ASF/sam0/utils/cmsis/samda1/source/system_samda1.c \
+ ../src/ASF/sam0/utils/cmsis/samda1/include/samda1.h \
+ ../src/ASF/sam0/utils/cmsis/samda1/include/samda1j16a.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\arm\arm-gnu-toolchain\lib\gcc\arm-none-eabi\6.3.1\include\stdint.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\arm\arm-gnu-toolchain\arm-none-eabi\include\stdint.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\arm\arm-gnu-toolchain\arm-none-eabi\include\machine\_default_types.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\arm\arm-gnu-toolchain\arm-none-eabi\include\sys\features.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\arm\arm-gnu-toolchain\arm-none-eabi\include\_newlib_version.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\arm\arm-gnu-toolchain\arm-none-eabi\include\sys\_intsup.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\arm\arm-gnu-toolchain\arm-none-eabi\include\sys\_stdint.h \
+ ../src/ASF/thirdparty/CMSIS/Include/core_cm0plus.h \
+ ../src/ASF/thirdparty/CMSIS/Include/cmsis_version.h \
+ ../src/ASF/thirdparty/CMSIS/Include/cmsis_compiler.h \
+ ../src/ASF/thirdparty/CMSIS/Include/cmsis_gcc.h \
+ ../src/ASF/sam0/utils/cmsis/samda1/source/system_samda1.h \
+ ../src/ASF/sam0/utils/cmsis/samda1/include/component/ac.h \
+ ../src/ASF/sam0/utils/cmsis/samda1/include/component/adc.h \
+ ../src/ASF/sam0/utils/cmsis/samda1/include/component/dac.h \
+ ../src/ASF/sam0/utils/cmsis/samda1/include/component/dmac.h \
+ ../src/ASF/sam0/utils/cmsis/samda1/include/component/dsu.h \
+ ../src/ASF/sam0/utils/cmsis/samda1/include/component/eic.h \
+ ../src/ASF/sam0/utils/cmsis/samda1/include/component/evsys.h \
+ ../src/ASF/sam0/utils/cmsis/samda1/include/component/gclk.h \
+ ../src/ASF/sam0/utils/cmsis/samda1/include/component/hmatrixb.h \
+ ../src/ASF/sam0/utils/cmsis/samda1/include/component/i2s.h \
+ ../src/ASF/sam0/utils/cmsis/samda1/include/component/mtb.h \
+ ../src/ASF/sam0/utils/cmsis/samda1/include/component/nvmctrl.h \
+ ../src/ASF/sam0/utils/cmsis/samda1/include/component/pac.h \
+ ../src/ASF/sam0/utils/cmsis/samda1/include/component/pm.h \
+ ../src/ASF/sam0/utils/cmsis/samda1/include/component/port.h \
+ ../src/ASF/sam0/utils/cmsis/samda1/include/component/rtc.h \
+ ../src/ASF/sam0/utils/cmsis/samda1/include/component/sercom.h \
+ ../src/ASF/sam0/utils/cmsis/samda1/include/component/sysctrl.h \
+ ../src/ASF/sam0/utils/cmsis/samda1/include/component/tc.h \
+ ../src/ASF/sam0/utils/cmsis/samda1/include/component/tcc.h \
+ ../src/ASF/sam0/utils/cmsis/samda1/include/component/usb.h \
+ ../src/ASF/sam0/utils/cmsis/samda1/include/component/wdt.h \
+ ../src/ASF/sam0/utils/cmsis/samda1/include/instance/ac.h \
+ ../src/ASF/sam0/utils/cmsis/samda1/include/instance/adc.h \
+ ../src/ASF/sam0/utils/cmsis/samda1/include/instance/dac.h \
+ ../src/ASF/sam0/utils/cmsis/samda1/include/instance/dmac.h \
+ ../src/ASF/sam0/utils/cmsis/samda1/include/instance/dsu.h \
+ ../src/ASF/sam0/utils/cmsis/samda1/include/instance/eic.h \
+ ../src/ASF/sam0/utils/cmsis/samda1/include/instance/evsys.h \
+ ../src/ASF/sam0/utils/cmsis/samda1/include/instance/gclk.h \
+ ../src/ASF/sam0/utils/cmsis/samda1/include/instance/sbmatrix.h \
+ ../src/ASF/sam0/utils/cmsis/samda1/include/instance/i2s.h \
+ ../src/ASF/sam0/utils/cmsis/samda1/include/instance/mtb.h \
+ ../src/ASF/sam0/utils/cmsis/samda1/include/instance/nvmctrl.h \
+ ../src/ASF/sam0/utils/cmsis/samda1/include/instance/pac0.h \
+ ../src/ASF/sam0/utils/cmsis/samda1/include/instance/pac1.h \
+ ../src/ASF/sam0/utils/cmsis/samda1/include/instance/pac2.h \
+ ../src/ASF/sam0/utils/cmsis/samda1/include/instance/pm.h \
+ ../src/ASF/sam0/utils/cmsis/samda1/include/instance/port.h \
+ ../src/ASF/sam0/utils/cmsis/samda1/include/instance/rtc.h \
+ ../src/ASF/sam0/utils/cmsis/samda1/include/instance/sercom0.h \
+ ../src/ASF/sam0/utils/cmsis/samda1/include/instance/sercom1.h \
+ ../src/ASF/sam0/utils/cmsis/samda1/include/instance/sercom2.h \
+ ../src/ASF/sam0/utils/cmsis/samda1/include/instance/sercom3.h \
+ ../src/ASF/sam0/utils/cmsis/samda1/include/instance/sercom4.h \
+ ../src/ASF/sam0/utils/cmsis/samda1/include/instance/sercom5.h \
+ ../src/ASF/sam0/utils/cmsis/samda1/include/instance/sysctrl.h \
+ ../src/ASF/sam0/utils/cmsis/samda1/include/instance/tc3.h \
+ ../src/ASF/sam0/utils/cmsis/samda1/include/instance/tc4.h \
+ ../src/ASF/sam0/utils/cmsis/samda1/include/instance/tc5.h \
+ ../src/ASF/sam0/utils/cmsis/samda1/include/instance/tc6.h \
+ ../src/ASF/sam0/utils/cmsis/samda1/include/instance/tc7.h \
+ ../src/ASF/sam0/utils/cmsis/samda1/include/instance/tcc0.h \
+ ../src/ASF/sam0/utils/cmsis/samda1/include/instance/tcc1.h \
+ ../src/ASF/sam0/utils/cmsis/samda1/include/instance/tcc2.h \
+ ../src/ASF/sam0/utils/cmsis/samda1/include/instance/usb.h \
+ ../src/ASF/sam0/utils/cmsis/samda1/include/instance/wdt.h \
+ ../src/ASF/sam0/utils/cmsis/samda1/include/pio/samda1j16a.h
+
+../src/ASF/sam0/utils/cmsis/samda1/include/samda1.h:
+
+../src/ASF/sam0/utils/cmsis/samda1/include/samda1j16a.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\arm\arm-gnu-toolchain\lib\gcc\arm-none-eabi\6.3.1\include\stdint.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\arm\arm-gnu-toolchain\arm-none-eabi\include\stdint.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\arm\arm-gnu-toolchain\arm-none-eabi\include\machine\_default_types.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\arm\arm-gnu-toolchain\arm-none-eabi\include\sys\features.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\arm\arm-gnu-toolchain\arm-none-eabi\include\_newlib_version.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\arm\arm-gnu-toolchain\arm-none-eabi\include\sys\_intsup.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\arm\arm-gnu-toolchain\arm-none-eabi\include\sys\_stdint.h:
+
+../src/ASF/thirdparty/CMSIS/Include/core_cm0plus.h:
+
+../src/ASF/thirdparty/CMSIS/Include/cmsis_version.h:
+
+../src/ASF/thirdparty/CMSIS/Include/cmsis_compiler.h:
+
+../src/ASF/thirdparty/CMSIS/Include/cmsis_gcc.h:
+
+../src/ASF/sam0/utils/cmsis/samda1/source/system_samda1.h:
+
+../src/ASF/sam0/utils/cmsis/samda1/include/component/ac.h:
+
+../src/ASF/sam0/utils/cmsis/samda1/include/component/adc.h:
+
+../src/ASF/sam0/utils/cmsis/samda1/include/component/dac.h:
+
+../src/ASF/sam0/utils/cmsis/samda1/include/component/dmac.h:
+
+../src/ASF/sam0/utils/cmsis/samda1/include/component/dsu.h:
+
+../src/ASF/sam0/utils/cmsis/samda1/include/component/eic.h:
+
+../src/ASF/sam0/utils/cmsis/samda1/include/component/evsys.h:
+
+../src/ASF/sam0/utils/cmsis/samda1/include/component/gclk.h:
+
+../src/ASF/sam0/utils/cmsis/samda1/include/component/hmatrixb.h:
+
+../src/ASF/sam0/utils/cmsis/samda1/include/component/i2s.h:
+
+../src/ASF/sam0/utils/cmsis/samda1/include/component/mtb.h:
+
+../src/ASF/sam0/utils/cmsis/samda1/include/component/nvmctrl.h:
+
+../src/ASF/sam0/utils/cmsis/samda1/include/component/pac.h:
+
+../src/ASF/sam0/utils/cmsis/samda1/include/component/pm.h:
+
+../src/ASF/sam0/utils/cmsis/samda1/include/component/port.h:
+
+../src/ASF/sam0/utils/cmsis/samda1/include/component/rtc.h:
+
+../src/ASF/sam0/utils/cmsis/samda1/include/component/sercom.h:
+
+../src/ASF/sam0/utils/cmsis/samda1/include/component/sysctrl.h:
+
+../src/ASF/sam0/utils/cmsis/samda1/include/component/tc.h:
+
+../src/ASF/sam0/utils/cmsis/samda1/include/component/tcc.h:
+
+../src/ASF/sam0/utils/cmsis/samda1/include/component/usb.h:
+
+../src/ASF/sam0/utils/cmsis/samda1/include/component/wdt.h:
+
+../src/ASF/sam0/utils/cmsis/samda1/include/instance/ac.h:
+
+../src/ASF/sam0/utils/cmsis/samda1/include/instance/adc.h:
+
+../src/ASF/sam0/utils/cmsis/samda1/include/instance/dac.h:
+
+../src/ASF/sam0/utils/cmsis/samda1/include/instance/dmac.h:
+
+../src/ASF/sam0/utils/cmsis/samda1/include/instance/dsu.h:
+
+../src/ASF/sam0/utils/cmsis/samda1/include/instance/eic.h:
+
+../src/ASF/sam0/utils/cmsis/samda1/include/instance/evsys.h:
+
+../src/ASF/sam0/utils/cmsis/samda1/include/instance/gclk.h:
+
+../src/ASF/sam0/utils/cmsis/samda1/include/instance/sbmatrix.h:
+
+../src/ASF/sam0/utils/cmsis/samda1/include/instance/i2s.h:
+
+../src/ASF/sam0/utils/cmsis/samda1/include/instance/mtb.h:
+
+../src/ASF/sam0/utils/cmsis/samda1/include/instance/nvmctrl.h:
+
+../src/ASF/sam0/utils/cmsis/samda1/include/instance/pac0.h:
+
+../src/ASF/sam0/utils/cmsis/samda1/include/instance/pac1.h:
+
+../src/ASF/sam0/utils/cmsis/samda1/include/instance/pac2.h:
+
+../src/ASF/sam0/utils/cmsis/samda1/include/instance/pm.h:
+
+../src/ASF/sam0/utils/cmsis/samda1/include/instance/port.h:
+
+../src/ASF/sam0/utils/cmsis/samda1/include/instance/rtc.h:
+
+../src/ASF/sam0/utils/cmsis/samda1/include/instance/sercom0.h:
+
+../src/ASF/sam0/utils/cmsis/samda1/include/instance/sercom1.h:
+
+../src/ASF/sam0/utils/cmsis/samda1/include/instance/sercom2.h:
+
+../src/ASF/sam0/utils/cmsis/samda1/include/instance/sercom3.h:
+
+../src/ASF/sam0/utils/cmsis/samda1/include/instance/sercom4.h:
+
+../src/ASF/sam0/utils/cmsis/samda1/include/instance/sercom5.h:
+
+../src/ASF/sam0/utils/cmsis/samda1/include/instance/sysctrl.h:
+
+../src/ASF/sam0/utils/cmsis/samda1/include/instance/tc3.h:
+
+../src/ASF/sam0/utils/cmsis/samda1/include/instance/tc4.h:
+
+../src/ASF/sam0/utils/cmsis/samda1/include/instance/tc5.h:
+
+../src/ASF/sam0/utils/cmsis/samda1/include/instance/tc6.h:
+
+../src/ASF/sam0/utils/cmsis/samda1/include/instance/tc7.h:
+
+../src/ASF/sam0/utils/cmsis/samda1/include/instance/tcc0.h:
+
+../src/ASF/sam0/utils/cmsis/samda1/include/instance/tcc1.h:
+
+../src/ASF/sam0/utils/cmsis/samda1/include/instance/tcc2.h:
+
+../src/ASF/sam0/utils/cmsis/samda1/include/instance/usb.h:
+
+../src/ASF/sam0/utils/cmsis/samda1/include/instance/wdt.h:
+
+../src/ASF/sam0/utils/cmsis/samda1/include/pio/samda1j16a.h:
